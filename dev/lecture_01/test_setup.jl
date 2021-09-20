516ed82653ac9435fb2b20cdbaa9ddd3109a21da
Documenter.jl <documenter@juliadocs.github.io>
2021-09-20 12:47:02 +0000

    build based on 4ab9322

--- dev/lecture_01/test_setup.jl
+++ dev/lecture_01/test_setup.jl
@@ -6,8 +6,11 @@ println("Julia started from terminal without args: ", length(ARGS) == 0 ? "✔" 
 println("Running from the same folder as this script: ", isfile("./test_setup.jl") ? "✔" : "✗")
 println("Running Julia 1.6.0 or above: ", VERSION >= v"1.6.0" ? "✔" : "✗")
 
+name, mail = readchomp(`git config user.name`), readchomp(`git config user.email`)
+println("Git Config Username: ", length(name) > 0 ? "✔" : "✗")
+println("Git Config Email: ", length(mail) > 0 ? "✔" : "✗")
 
-envdir = "./L1Env/"
+envdir = "L1Env"
 s = if !isdir(envdir)
 	using Pkg
 
