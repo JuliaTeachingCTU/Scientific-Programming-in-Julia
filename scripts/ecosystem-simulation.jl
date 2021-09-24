5b7fe13629faf3a90239d90b1e1d049e8df97b03
Niklas <heimnikl@fel.cvut.cz>
2021-09-24 02:25:52 +0200

    update Ecosystem.jl; plots

--- scripts/ecosystem-simulation.jl
+++ scripts/ecosystem-simulation.jl
@@ -2,23 +2,25 @@ using Scientific_Programming_in_Julia
 using Random
 Random.seed!(4)
 
-n_sheep  = 100
-n_wolves = 8
-n_grass = 500
-regrowth_time = 17.0
-Δenergy_sheep = 5.0
-Δenergy_wolf = 17.0
-wolf_reproduce = 0.03
-sheep_reproduce = 0.5
-wolf_foodprob = 0.02
-sheep_foodprob = 0.4
+n_grass       = 1000
+regrowth_time = 21.0
 
-gs = [Grass(true,regrowth_time,regrowth_time) for _ in 1:n_grass]
-pgs = [PoisonedGrass(true,regrowth_time,regrowth_time) for _ in 1:1]
+n_sheep         = 10
+Δenergy_sheep   = 4.0
+sheep_reproduce = 0.3
+sheep_foodprob  = 0.6
+
+n_wolves       = 2
+Δenergy_wolf   = 9.0
+wolf_reproduce = 0.03
+wolf_foodprob  = 0.12
+
+gs = [Grass(id,true,regrowth_time,regrowth_time) for id in 1:n_grass]
+ss = [Sheep(id,2*Δenergy_sheep,Δenergy_sheep,sheep_reproduce, sheep_foodprob) for id in (n_grass+1):(n_grass+n_sheep)]
+ws = [Wolf(id,2*Δenergy_wolf,Δenergy_wolf,wolf_reproduce, wolf_foodprob) for id in (n_grass+n_sheep+1):(n_grass+n_sheep+n_wolves)]
+#pgs = [PoisonedGrass(true,regrowth_time,regrowth_time) for _ in (n_grass+1):(n_grass+1)]
 #ss = [Sheep(2*Δenergy_sheep,Δenergy_sheep,sheep_reproduce, sheep_foodprob) for _ in 1:n_sheep]
-ss = [GenderedSheep(2*Δenergy_sheep,Δenergy_sheep,sheep_reproduce, sheep_foodprob) for _ in 1:n_sheep]
-ws = [Wolf(2*Δenergy_wolf,Δenergy_wolf,wolf_reproduce, wolf_foodprob) for _ in 1:n_wolves]
-as = vcat(gs,pgs,ss,ws)
+as = vcat(gs,ss,ws)
 
 w = World(as)
 
@@ -33,13 +35,13 @@ logcb = every_nth(w->(@info agent_count(w)), 5)
 savecb = every_nth(_save, 1)
 cbs = [logcb, savecb]
 
-simulate!(w, 200, callbacks=cbs)
+simulate!(w, 500, callbacks=cbs)
 
 using Plots
 plt = plot()
 for (n,c) in counts
     plot!(plt, c, label="$n", lw=2)
 end
-# savefig(plt, "docs/src/lecture_02/pred-prey.png")
+savefig(plt, "docs/src/lecture_02/pred-prey.png")
 # savefig(plt, "docs/src/lecture_03/pred-prey.png")
 display(plt)
--- src/Ecosystem.jl
+++ src/Ecosystem.jl
@@ -6,13 +6,13 @@ using EcosystemCore
 export Grass, Sheep, Wolf, World, PoisonedGrass, ⚥Sheep
 export agent_step!, agent_count, simulate!, every_nth
 
-mutable struct PoisonedGrass <: AbstractPlant
+mutable struct PoisonedGrass <: Plant
+    id::Int
     fully_grown::Bool
     regrowth_time::Int
     countdown::Int
 end
-PoisonedGrass(t) = PoisonedGrass(false, t, rand(1:t))
-PoisonedGrass() = PoisonedGrass(2)
+PoisonedGrass(id,t) = PoisonedGrass(id, false, t, rand(1:t))
 
 EcosystemCore.eats(::Sheep,::PoisonedGrass) = true
 
@@ -24,29 +24,27 @@ function EcosystemCore.eat!(sheep::Sheep, grass::PoisonedGrass, w::World)
 end
 
 
-struct ⚥Sheep{T<:Real} <: AbstractAnimal
-    sheep::Sheep{T}
-    gender::Symbol
+struct ⚥Sheep <: Animal
+    sheep::Sheep
+    sex::Symbol
 end
-⚥Sheep(E,ΔE,pr,pf,gender) = ⚥Sheep(Sheep(E,ΔE,pr,pf),gender)
-⚥Sheep(E,ΔE,pr,pf) = ⚥Sheep(E,ΔE,pr,pf,sample([:male,:female]))
+⚥Sheep(id,E,ΔE,pr,pf,sex) = ⚥Sheep(Sheep(id,E,ΔE,pr,pf),sex)
 
+EcosystemCore.id(g::⚥Sheep) = EcosystemCore.id(g.sheep)
 EcosystemCore.energy(g::⚥Sheep) = energy(g.sheep)
-EcosystemCore.energy!(g::⚥Sheep, e) = energy!(g.sheep, e)
-EcosystemCore.Δenergy(g::⚥Sheep, e) = Δenergy(g.sheep, e)
-EcosystemCore.incr_energy!(g::⚥Sheep, Δe) = incr_energy!(g.sheep, Δe)
+EcosystemCore.energy!(g::⚥Sheep, ΔE) = energy!(g.sheep, ΔE)
 EcosystemCore.reproduction_prob(g::⚥Sheep) = reproduction_prob(g.sheep)
 EcosystemCore.food_prob(g::⚥Sheep) = food_prob(g.sheep)
 
 EcosystemCore.eats(::⚥Sheep, ::Grass) = true
-EcosystemCore.eats(::⚥Sheep, ::PoisonedGrass) = true
-EcosystemCore.eat!(s::⚥Sheep, g::AbstractPlant, w::World) = eat!(s.sheep, g, w)
+# eats(::⚥Sheep, ::PoisonedGrass) = true
+EcosystemCore.eat!(s::⚥Sheep, g::Plant, w::World) = eat!(s.sheep, g, w)
 
-mates(a::AbstractPlant, ::⚥Sheep) = false
-mates(a::AbstractAnimal, ::⚥Sheep) = false
-mates(g1::⚥Sheep, g2::⚥Sheep) = g1.gender != g2.gender
+mates(a::Plant, ::⚥Sheep) = false
+mates(a::Animal, ::⚥Sheep) = false
+mates(g1::⚥Sheep, g2::⚥Sheep) = g1.sex != g2.sex
 function find_mate(g::⚥Sheep, w::World)
-    ms = filter(a->mates(a,g), w.agents)
+    ms = filter(a->mates(a,g), w.agents |> values |> collect)
     isempty(ms) ? nothing : sample(ms)
 end
 
@@ -54,27 +52,31 @@ function EcosystemCore.reproduce!(s::⚥Sheep, w::World)
     m = find_mate(s,w)
     if !isnothing(m)
         energy!(s, energy(s)/2)
-        # TODO: should probably mix s/m
-        push!(w.agents, deepcopy(s))
+        vals = [getproperty(s.sheep,n) for n in fieldnames(Sheep) if n!=:id]
+        new_id = w.max_id + 1
+        ŝ = ⚥Sheep(new_id, vals..., rand(Bool) ? :female : :male)
+        w.agents[EcosystemCore.id(ŝ)] = ŝ
+        w.max_id = new_id
     end
 end
 
 
 
-countsym(g::T) where T<:AbstractPlant = fully_grown(g) ? nameof(T) : :NoCount
-countsym(::T) where T<:AbstractAnimal = nameof(T)
 
-# function agent_count(as::Vector{AbstractAgent})
+countsym(g::T) where T<:Plant = fully_grown(g) ? nameof(T) : :NoCount
+countsym(::T) where T<:Animal = nameof(T)
+
+# function agent_count(as::Vector{Agent})
 #     cs = StatsBase.countmap(map(countsym,as))
 #     delete!(cs,:NoCount)
 # end
 
-agent_count(g::AbstractPlant) = fully_grown(g) ? 1 : 0
-agent_count(::AbstractAnimal) = 1
-agent_count(as::Vector{<:AbstractAgent}) = sum(agent_count,as)
+agent_count(g::Plant) = fully_grown(g) ? 1 : 0
+agent_count(::Animal) = 1
+agent_count(as::Vector{<:Agent}) = sum(agent_count,as)
 
 function agent_count(w::World)
-    function op(d::Dict,a::T) where T<:AbstractAgent
+    function op(d::Dict,a::T) where T<:Agent
         n = nameof(T)
         if n in keys(d)
             d[n] += agent_count(a)
@@ -83,18 +85,7 @@ function agent_count(w::World)
         end
         return d
     end
-    foldl(op, w.agents, init=Dict{Symbol,Int}())
-end
-
-function simulate!(w::World, iters::Int; callbacks=[])
-    for i in 1:iters
-        for a in w.agents
-            agent_step!(a,w)
-        end
-        for cb in callbacks
-            cb(w)
-        end
-    end
+    foldl(op, w.agents |> values |> collect, init=Dict{Symbol,Int}())
 end
 
 function every_nth(f::Function, n::Int)
