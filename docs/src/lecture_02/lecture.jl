c5234d6e7607b14eab910b2f2c6e5243e549b43a
Niklas <heimnikl@fel.cvut.cz>
2021-10-07 14:03:36 +0200

    finish going through lectures

--- docs/src/lecture_02/lecture.jl
+++ docs/src/lecture_02/lecture.jl
@@ -62,6 +62,7 @@ b = (Wolf("1", 1), Wolf("2", 2), Sheep("3", 3))
 # and the third item is of type `Sheep`. Moreover, consider a function which calculates the
 # energy of all animals as 
 energy(animals) = mapreduce(x -> x.energy, +, animals)
+nothing # hide
 # A good compiler makes use of the information provided by the type system to generate effiecint code
 # which we can verify by inspecting the compiled code using `@code_native` macro
 @code_native energy(a)
@@ -217,13 +218,13 @@ struct LessVaguePosition
 end
 z = [LessVaguePosition(rand(), rand()) for _ in 1:100];
 @benchmark reduce(move, z)
-# While from the perspective of generating optimal code, both definitions are equally informative to the compiler as it cannot assume anything about the code, the  `LessVaguePosition` will ensure that the position will contain only numbers, hence catching trivial errors like instantiating `VaguePosition` with non-numeric types for which arithmetic operators will not be defined (recall the discussion on the  beggining) of the lecture. 
+# From the perspective of generating optimal code, both definitions are equally uninformative to the compiler as it cannot assume anything about the code. However, the  `LessVaguePosition` will ensure that the position will contain only numbers, hence catching trivial errors like instantiating `VaguePosition` with non-numeric types for which arithmetic operators will not be defined (recall the discussion on the  beginning of the lecture). 
 
 # All structs defined above are immutable (as we have seen above in the case of `Tuple`), which means that one cannot change a field (unless the struct wraps a container, like and array, which allows that). For example this raises an error
-a = Position(1, 2)
+a = LessVaguePosition(1,2)
 a.x = 2
 
-# If one needs to make a struct mutable, use the keyword `mutable` befoer keyword `struct` as
+# If one needs to make a struct mutable, use the keyword `mutable` before the keyword `struct` as
 mutable struct MutablePosition
   x::Float64
   y::Float64
@@ -231,6 +232,7 @@ end
 # In mutable structures, we can change the values of fields.
 a = MutablePosition(1e0, 2e0)
 a.x = 2
+a
 # Note, that the memory layout of mutable structures is different, as fields now contain references to memory locations, where the actual values are stored. 
 
 # ### Parametric Types
@@ -241,15 +243,15 @@ struct PositionT{T}
 end
 u = [PositionT(rand(), rand()) for _ in 1:100];
 @btime reduce(move, u);
-# Notice that the compiler can take advantage of specializing for different types (which does not have effect as in modern processrs have addition of `Float` and `Int` takes the same time).
+# Notice that the compiler can take advantage of specializing for different types (which does not have an effect here as in modern processors addition of `Float` and `Int` takes the same time).
 v = [PositionT(rand(1:100), rand(1:100)) for _ in 1:100];
 @btime reduce(move, v);
-# The above definition suffers the same problem as `VaguePosition`, which is that it allows us to instantiate the `PositionT` with non-numeric types, e.g. `String`. We solve this by restricting the types `T` to be childs of some supertype, in this case `Real`
+# The above definition suffers the same problem as `VaguePosition`, which is that it allows us to instantiate the `PositionT` with non-numeric types, e.g. `String`. We solve this by restricting the types `T` to be children of some supertype, in this case `Real`
 struct Position{T<:Real}
   x::T
   y::T 
 end
-# which will throw error if we try to initialize it with `Position("1.0", "2.0")`. 
+# which will throw an error if we try to initialize it with `Position("1.0", "2.0")`. 
 # 
 # Naturally, fields in structures can be of different types, as is in the below pointless example.
 struct PositionXY{X<:Real, Y<:Real}
@@ -258,68 +260,71 @@ struct PositionXY{X<:Real, Y<:Real}
 end
 
 # ### Abstract parametric types
-# Like Composite types, Abstract types can also have parameters. These parameters defines types that are common for all child types. A very good example is Julia's definition of arrays of arbitrary dimension `N` and type `T` of its items as
+# Like Composite types, Abstract types can also have parameters. These parameters define types that are common for all child types. A very good example is Julia's definition of arrays of arbitrary dimension `N` and type `T` of its items as
 # ```julia
 # abstract type AbstractArray{T,N} end
 # ```
-# Different `T` and `N` gives rise to different variants of `AbstractArrays`, 
+# Different `T` and `N` give rise to different variants of `AbstractArrays`, 
 # therefore `AbstractArray{Float32,2}` is different from `AbstractArray{Float64,2}` 
 # and from `AbstractArray{Float64,1}.` Note that these are still `Abstract` types, 
-# which means you cannot instantiate them. They purpose is
+# which means you cannot instantiate them. Their purpose is
 # * to allow to define operations for broad class of concrete types
-# * to inform compiler about constant values, which can be used 
-# Notice in the above example that parameters of types do not have to be types, but also values of primitive types, as in the above example of `AbstractArray` `N` is the number of dimensions which is an integer value.
+# * to inform the compiler about constant values, which can be used 
+# Notice in the above example that parameters of types do not have to be types, but can also be values of primitive types, as in the above example of `AbstractArray` `N` is the number of dimensions which is an integer value.
 # 
 # For convenience, it is common to give some important partially instantiated Abstract types an **alias**, for example `AbstractVector` as 
 # ```julia
 # const AbstractVector{T} = AbstractArray{T,1}
 # ```
-# is defined in `array.jl:23` (in Julia 1.6.2), which allows us to define for example general prescription for `dot` product of two abstract vectors as 
+# is defined in `array.jl:23` (in Julia 1.6.2), which allows us to define for example general prescription for the `dot` product of two abstract vectors as 
 function dot(a::AbstractVector, b::AbstractVector)
   @assert length(a) == length(b)
   mapreduce(*, +, a, b)
 end
-# You can verify that the above general function can be compiled to a performant code if 
-# specialized for a particular arguments.
+# You can verify that the above general function can be compiled to performant code if 
+# specialized for particular arguments.
 @code_native mapreduce(*,+, [1,2,3], [1,2,3])
 
 
-# ## More on use of types in function definitions
+# ## More on the use of types in function definitions
 # ### Terminology
-# * A *function* refers to a set of "methods" for a different combination of type parameters (a term function can be therefore considered as refering to a mere **name**). *Methods* define different behavior for different type of arguments for a given function. For in below example
+# * A *function* refers to a set of "methods" for a different combination of type parameters (the term function can be therefore considered as refering to a mere **name**). *Methods* define different behavior for different types of arguments for a given function. For example
 move(a::Position, b::Position) = Position(a.x + b.x, a.y + b.y)
 move(a::Vector{<:Position}, b::Vector{<:Position}) = move.(a,b)
-# `move` refers to a function with methods `move(a::Position, b::Position)` and `move(a::Vector{<:Position}, b::Vector{<:Position})`. When different behavior on different types is defined by a programmer, as shown above, it is also called *implementation specialization*. There is another type of specialization, called compiler specialization*, which occurs when the compiler generates different functions for you from a single method. For example for 
+# `move` refers to a function with methods `move(a::Position, b::Position)` and `move(a::Vector{<:Position}, b::Vector{<:Position})`. When different behavior on different types is defined by a programmer, as shown above, it is also called *implementation specialization*. There is another type of specialization, called *compiler specialization*, which occurs when the compiler generates different functions for you from a single method. For example for 
 move(Position(1,1), Position(2,2))
 move(Position(1.0,1.0), Position(2.0,2.0))
 # the compiler generates two methods, one for `Position{Int64}` and the other for `Position{Float64}`. Notice that inside generated functions, the compiler needs to use different intrinsic operations, which can be viewed from 
-@code_native  move(Position(1,1), Position(2,2))
+@code_native move(Position(1,1), Position(2,2))
 # and 
 @code_native move(Position(1.0,1.0), Position(2.0,2.0))
 
-# ## Intermezzo: How Julia compiler works?
-# Let's walk through an example. Consider following definitions
+# ## Intermezzo: How does the Julia compiler work?
+# Let's walk through an example. Consider the following definitions
 move(a::Position, by::Position) = Position(a.x + by.x, a.y + by.y)
 move(a::T, by::T) where {T<:Position} = Position(a.x + by.x, a.y + by.y)
 move(a::Position{Float64}, by::Position{Float64}) = Position(a.x + by.x, a.y + by.y)
 move(a::Vector{<:Position}, by::Vector{<:Position}) = move.(a, by)
 move(a::Vector{<:Position}, by::Position) = move.(a, by)
+nothing # hide
 # and a function call 
 a = Position(1.0, 1.0)
 by = Position(2.0, 2.0)
 move(a, by)
-# 1. The compiler knows that you call function `move` 
-# 2. and the compiler infers type of arguments (you can see the result using 
-    (typeof(a),typeof(by))
-# 3. The compiler identifies all methods that can be applied to a function `move` with arguments of type `(Position{Float64}, Position{Float64})`
-    Base.method_instances(move, (typeof(a), typeof(by)))
-    m = Base.method_instances(move, (typeof(a), typeof(by))) |> first
+# 1. The compiler knows that you call the function `move`.
+# 2. The compiler infers the type of the arguments. You can view the result with
+(typeof(a),typeof(by))
+# 3. The compiler identifies all `move`-methods with arguments of type `(Position{Float64}, Position{Float64})`:
+Base.method_instances(move, (typeof(a), typeof(by)))
+# (For demonstration we can store the method we want in a variable `m`)
+m = Base.method_instances(move, (typeof(a), typeof(by))) |> first
+nothing # hide
 # 4a. If the method has been specialized (compiled), then the arguments are prepared and the method is invoked. The compiled specialization can be seen from 
-  m.cache
-# 4b. If the method has not been specialized (compiled), the compiler compiles the method for a given type of arguments  and continues as in step 4a.
-# A compiled function is therefore  a "blob" of **native code** living in a particular memory location. When Julia calls a function, it needs to pick a right block corresponding to a function with particular type of parameters.
+m.cache
+# 4b. If the method has not been specialized (compiled), the method is compiled for the given type of arguments and continues as in step 4a.
+# A compiled function is therefore  a "blob" of **native code** living in a particular memory location. When Julia calls a function, it needs to pick the right block corresponding to a function with particular type of parameters.
 #
-# If the compiler cannot narrow types of arguments to concrete types, it has to perform the above procedure inside the called function, which has negative effect on the performance, as the type resulution and identification of the method can be slow, especially for methods with many arguments (e.g. 30ns for a method with one argument, 
+# If the compiler cannot narrow the types of arguments to concrete types, it has to perform the above procedure inside the called function, which has negative effects on performance, as the type resulution and identification of the methods can be slow, especially for methods with many arguments (e.g. 30ns for a method with one argument, 
 # 100 ns for method with two arguements).
 # Recall the above example 
 wolfpack_a =  [Wolf("1", 1), Wolf("2", 2), Wolf("3", 3)]
@@ -341,14 +346,14 @@ wolfpack_b =  Any[Wolf("1", 1), Wolf("2", 2), Wolf("3", 3)]
 const WolfOrSheep = Union{Wolf, Sheep}
 wolfpack_c =  WolfOrSheep[Wolf("1", 1), Wolf("2", 2), Wolf("3", 3)]
 @benchmark energy(wolfpack_c)
-# THanks to union splitting, Julia is able to have performant operations on arrays with undefined / missing values for example 
+# Thanks to union splitting, Julia is able to have performant operations on arrays with undefined / missing values for example 
 [1, 2, 3, missing] |> typeof
 
 # ### More on matching methods and arguments
-# In the above process, the step, where Julia looks for a method instance with corresponding parameters can be very confusing. The rest of this lecture will focus on this. For those who want to have a formal background, we recommend (talk of  Francesco Zappa Nardelli)[https://www.youtube.com/watch?v=Y95fAipREHQ] and / or the that of (Jan Vitek)[https://www.youtube.com/watch?v=LT4AP7CUMAw].
+# In the above process, the step, where Julia looks for a method instance with corresponding parameters can be very confusing. The rest of this lecture will focus on this. For those who want to have a formal background, we recommend [talk of  Francesco Zappa Nardelli](https://www.youtube.com/watch?v=Y95fAipREHQ) and / or the one of [Jan Vitek](https://www.youtube.com/watch?v=LT4AP7CUMAw).
 # 
-# When Julia needs to specialize a method instance, in needs to find it among multiple definitions. A single function can have many method instances, see for example `methods(+)` which  lists all methods instances of `+` function. How Julia select the proper one?
-# 1. It finds all methods where type of arguments match or are subtypes of restrictions  on arguments in the method definition.
+# When Julia needs to specialize a method instance, it needs to find it among multiple definitions. A single function can have many method instances, see for example `methods(+)` which  lists all method instances of the `+`-function. How does Julia select the proper one?
+# 1. It finds all methods where the type of arguments match or are subtypes of restrictions on arguments in the method definition.
 # 2a. If there are multiple matches, the compiler selects the most specific definition. 
 # 2b. If the compiler cannot decide, which method instance to choose, it throws an error. 
 confused_move(a::Position{Float64}, by) = Position(a.x + by.x, a.y + by.y)
@@ -367,30 +372,29 @@ move(a::Vector{T}, by::Vector{T}) where {T<:Position} = move.(a, by)
 move(a::Vector{<:Position}, by::Position) = move.(a, by)
 # Which method will compiler select for 
 move(Position(1.0,2.0), Position(1.0,2.0))
-# First three matches types of argumens, but the compiler will select the third one, since it is the most specific.
+# The first three methods match the types of argumens, but the compiler will select the third one, since it is the most specific.
 #
 # Which method will compiler select for 
 move(Position(1,2), Position(1,2))
 # Again, the first and second method definitions match the argument, but the second is the most specific.
 #
-# Which method will compiler select for 
+# Which method will the compiler select for 
 move([Position(1,2)], [Position(1,2)])
 # Again, the fourth and fifth method definitions match the argument, but the fifth is the most specific.
 move([Position(1,2), Position(1.0,2.0)], [Position(1,2), Position(1.0,2.0)])
 
 # ### Frequent problems
-# 1. Why the following fails?
+# 1. Why does the following fail?
 foo(a::Vector{Real}) = println("Vector{Real}")
 foo([1.0,2,3])
-#   Julia's type system is **invariant**, which means that `Vector{Real}` is different from `Vector{Float64}` and from `Vector{Float32}`, even though `Float64` and `Float32` are sub-types of `Real`. Therefore `typeof([1.0,2,3])` isa `Vector{Float64}` which is not subtype of `Vector{Real}.` For **covariant** languages, this would be true. For more information on variance in computer languages, see ()[https://en.wikipedia.org/wiki/Covariance_and_contravariance_(computer_science)]. If de above definition of `foo` should be applicable to all vectors which has elements of subtype of `Real` we have define it as 
+# Julia's type system is **invariant**, which means that `Vector{Real}` is different from `Vector{Float64}` and from `Vector{Float32}`, even though `Float64` and `Float32` are sub-types of `Real`. Therefore `typeof([1.0,2,3])` isa `Vector{Float64}` which is not subtype of `Vector{Real}.` For **covariant** languages, this would be true. For more information on variance in computer languages, [see here](https://en.wikipedia.org/wiki/Covariance_and_contravariance_(computer_science)). If de above definition of `foo` should be applicable to all vectors which has elements of subtype of `Real` we have define it as 
 foo(a::Vector{T}) where {T<:Real} = println("Vector{T} where {T<:Real}")
-#   or equivalently but more tersely as 
+# or equivalently but more tersely as 
 foo(a::Vector{<:Real}) = println("Vector{T} where {T<:Real}")
-# 2. Diagonal rule
-# rule says that the type repeat in method signature, it has to be a concrete type. Consider for example the function below
+# 2. Diagonal rule says that a repeated type in a method signature has to be a concrete type. Consider for example the function below
 move(a::T, b::T) where {T<:Position}
 # we cannot call it with `move(Position(1.0,2.0), Position(1,2))`, since in this case `Position(1.0,2.0)` is of type `Position{Float64}` while `Position(1,2)` is of type `Position{Int64}`.
-# 3. When debugging why arguments does not match the particular method definition, it is useful to use `typeof`, `isa`, and `<:` commands. For example 
+# 3. When debugging why arguments do not match a particular method definition, it is useful to use `typeof`, `isa`, and `<:` commands. For example 
 typeof(Position(1.0,2.0))
 #
 typeof(Position(1,2))
@@ -406,11 +410,11 @@ typeof(Position(1,2)) <: Position{<:Float64}
 typeof(Position(1,2)) <: Position{<:Real}
 
 
-# ### A bizzare definitions which you can encounter
-# A following definition of One-Hot Matrix is taken from Flux.jl
+# ### A bizzare definition which you can encounter
+# The following definition of a one-hot matrix is taken from [Flux.jl](https://github.com/FluxML/Flux.jl/blob/1a0b51938b9a3d679c6950eece214cd18108395f/src/onehot.jl#L10-L12)
 struct OneHotArray{T<:Integer, L, N, var"N+1", I<:Union{T,AbstractArray{T, N}}} <: AbstractArray{Bool, var"N+1"}
   indices::I
 end
-# The parameters of the type carry an information about the type used to encode position of `one` in each column in `T`), the dimension of one-hot vectors in `L`, the dimension of the storage of `indices` in `N` (which is zero for OneHotVector and one for OneHotMatrix), number of dimensions of the OneHotArray in `var"N+1"` and the type of underlying storage of indicies `I`.
+# The parameters of the type carry information about the type used to encode the position of `one` in each column in `T`, the dimension of one-hot vectors in `L`, the dimension of the storage of `indices` in `N` (which is zero for `OneHotVector` and one for `OneHotMatrix`), number of dimensions of the `OneHotArray` in `var"N+1"` and the type of underlying storage of indicies `I`.
 
 
