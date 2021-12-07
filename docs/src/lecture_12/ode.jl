286bffaa1d6e296dc6c18c3764af4f69047d2dc6
smidl <vasek.smidl@gmail.com>
2021-12-07 16:02:40 +0100

    Quadrics

--- docs/src/lecture_12/ode.jl
+++ docs/src/lecture_12/ode.jl
@@ -29,11 +29,11 @@ p=plot(X[1,:],xlabel="t",label="x",color=:blue)
 p=plot!(X[2,:],xlabel="t",label="y",color=:red)
 
 
-K=10
+K=100
 X0 = [x0 .+ 0.1*randn(2) for k=1:K]
 Xens=[X=solve(f,X0[i],θ0,dt,N) for i=1:K]
 
-for i=1:10
+for i=1:K
   p=plot!(Xens[i][1,:],label="",color=:blue)
   p=plot!(Xens[i][2,:],label="",color=:red)  
 end
@@ -103,29 +103,39 @@ savefig("LV_Measurements2.svg")
 # plot(Vector{GaussNum})
 
 using LinearAlgebra
-function solve(f,x0::AbstractVector,√Σ0, θ,dt,N)
-  const n = length(x0)
-  const n2 = 2*length(x0)
-  const Qp = sqrt(n2)*[I(n) -I(n)]
+function solve(f,x0::AbstractVector,sqΣ0, θ,dt,N)
+   n = length(x0)
+   n2 = 2*length(x0)
+   Qp = sqrt(n)*[I(n) -I(n)]
 
   X = hcat([zero(x0) for i=1:N]...)
   S = hcat([zero(x0) for i=1:N]...)
   X[:,1]=x0
-  √Σ = √Σ0
-  Σ = √Σ* √Σ'
+  sqΣ = sqΣ0
+  Σ = sqΣ* sqΣ'
   S[:,1]= diag(Σ)
   for t=1:N-1
-    Xp = x0 .+ √Σ * Qp
+    Xp = X[:,t] .+ sqΣ * Qp
     for i=1:n2 # all quadrature points
-      Xp.=X[:,t] + dt*f(Xp[:,i],θ)
+      Xp[:,i].=Xp[:,i] + dt*f(Xp[:,i],θ)
     end
     mXp=mean(Xp,dims=2)
     X[:,t+1]=mXp
-    Σ=(Xp.-mXp)*(Xp.-mXp)'/sqrt(n2)
+    Σ=Matrix((Xp.-mXp)*(Xp.-mXp)'/n2)
     S[:,t+1]=diag(Σ)
-    √Σ = chol(Σ)
+    # @show Σ
+    sqΣ = cholesky(Σ).L
+
   end
   X,S
 end
 
 ## Extension to arbitrary 
+
+QX,QS=solve(f,[1.0,1.0],sqrt(0.1)*I(2),θ0,0.1,1000)
+plot(QX[1,1:30:end],label="x",color=:blue,errorbar=QS[1,1:30:end])
+plot!(QX[2,1:30:end],label="y",color=:red,errorbar=QS[2,1:30:end])
+
+QX,QS=solve(f,[1.0,1.0],sqrt(0.1)*I(2),θ0,0.01,10000)
+plot(QX[1,1:100:end],label="x",color=:blue,errorbar=QS[1,1:100:end])
+plot!(QX[2,1:100:end],label="y",color=:red,errorbar=QS[2,1:100:end])
