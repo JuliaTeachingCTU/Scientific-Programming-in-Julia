ccd0f99ca1e2cad5760e3edd147e97a9f8dc55a2
Niklas Heim <heim.niklas@gmail.com>
2022-01-06 16:00:13 +0100

    clean up broadcasted NN

--- docs/src/lecture_08/reverse-nn-broadcast.jl
+++ docs/src/lecture_08/reverse-nn-broadcast.jl
@@ -3,17 +3,8 @@ using Base: tail
 
 import Scientific_Programming_in_Julia.ReverseDiff
 
-using Base: tail
-struct Dense{M<:TrackedMatrix, V<:TrackedVector, F}
-    W::M
-    b::V
-    f::F
-end
 
-
-Dense(W::Matrix, b::Vector, f) = Dense(track(W),track(b),f)
-Dense(in,out,f=identity) = Dense(rand(out,in) .-0.5, rand(out) .-0.5, f)
-
+##########  Broadcasting  ######################################################
 
 import Base.Broadcast: broadcasted
 
@@ -23,15 +14,57 @@ function broadcasted(::typeof(+), A::TrackedMatrix, B::TrackedVector)
     B.children[C] = Δ -> sum(Δ, dims = 2)[:]
     C
 end
-
-function (m::Dense)(x::AbstractMatrix)
-    m.f(m.W*x .+ m.b)
-end
-function (m::Dense)(x::AbstractVector)
-    m.f(m.W*x + m.b)
+function broadcasted(::typeof(identity), A::TrackedArray)
+    C = track(A.data)
+    A.children[C] = Δ->Δ
+    C
 end
 
+function broadcasted(::typeof(σ), x::TrackedArray)
+    z = track(σ.(x.data))
+    d = z.data
+    x.children[z] = Δ -> Δ .* d .* (1 .- d)
+    z
+end
+function broadcasted(::typeof(+), X::TrackedArray, Y::TrackedArray)
+    Z = track(X.data + Y.data)
+    X.children[Z] = Δ -> Δ
+    Y.children[Z] = Δ -> Δ
+    Z
+end
+function Base.:+(X::TrackedReal, Y::TrackedReal)
+    Z = track(X.data + Y.data)
+    X.children[Z] = Δ -> Δ
+    Y.children[Z] = Δ -> Δ
+    Z
+end
 
+# relu(x::Real) = max(0,x)
+# function relu(x::TrackedReal)
+#     y = track(relu(x.data))
+#     x.children[y] = Δ -> Δ > 0
+#     y
+# end
+# function broadcasted(::typeof(relu), A::TrackedArray)
+#     C = track(relu.(A.data))
+#     A.children[C] = Δ -> Δ .> 0
+#     C
+# end
+
+
+
+##########  Network definition  ################################################
+using Base: tail
+struct Dense{M<:TrackedMatrix, V<:TrackedVector, F}
+    W::M
+    b::V
+    f::F
+end
+
+Dense(W::Matrix, b::Vector, f) = Dense(track(W),track(b),f)
+Dense(in,out,f=identity) = Dense(rand(out,in) .-0.5, rand(out) .-0.5, f)
+
+(m::Dense)(x) = m.f.(m.W*x .+ m.b)
 
 struct Chain{T<:Tuple}
     layers::T
@@ -68,24 +101,6 @@ function train_step!(loss, network, xs, ys; λ=0.01)
 end
 
 
-# task
-f(x,y) = y^2 + sin(x)
-f(xs::AbstractVector) = [f(xs[1],xs[2])]
-f(xs::AbstractMatrix) = reshape(f.(xs[1,:],xs[2,:]), 1, :)
-
-using Random
-Random.seed!(0)
-
-## data
-#xs = map(1:30) do i
-#    x = rand(-4:0.1:4)
-#    y = rand(-2:0.1:2)
-#    [x,y]
-#end
-#xs = reduce(hcat, xs)
-#ys = track(f(xs))
-#xs = track(xs)
-
 function flower(n; npetals = 8)
     n = div(n, npetals)
     x = mapreduce(hcat, (1:npetals) .* (2π/npetals)) do θ
@@ -105,7 +120,6 @@ function flower(n; npetals = 8)
     for i in 1:length(_y)
         y[_y[i],i] = 1
     end
-
     #y = foreach(i -> y[_y[i], i] = 1, _y)
     #display(y)
     #error()
@@ -120,25 +134,33 @@ network = Chain(
     Dense(hdim,hdim,σ),
     Dense(hdim,size(ys,1))
 )
-function loss(network,xs,ys)
-    sum(abs2(network(xs) - ys))
-    # errs = hcat([abs2(network(x)-y) for (x,y) in zip(xs,ys)]...)
-    # sum(errs)
-end
 
-# l = loss(network,xs,ys)
-# l.grad = 1.0
-# display(l)
-# display(accum!(network.layers[1].W))
-# 
-# error()
+loss(network,xs,ys) = sum(abs2(network(xs) - ys))
 
 using Plots
 color_scheme = cgrad(:RdYlBu_5, rev=true)
 
-forward(network,x,y) = network(track([x,y])).data[1]
+# only for plotting
+forward(network,x,y) = _y = network(track([x,y])).data
 
-# training
+onecold(x::AbstractMatrix) = [y.I[1] for y in argmax(x,dims=1)]
+onecold(x::AbstractVector) = argmax(x)
+
+for i in 1:500
+    ReverseDiff.reset!(xs)
+    ReverseDiff.reset!(ys)
+    ReverseDiff.reset!(network)
+    l = train_step!(loss, network, xs, ys, λ=0.00005)
+    if mod1(i,30) == 1
+        @info i l
+
+        p1 = contour(-5:0.3:5, -5:0.2:5, (x,y)->onecold(forward(network,x,y)), fill=true, c=color_scheme)
+        scatter!(p1, xs[1,:].data, xs[2,:].data, c=onecold(ys.data)|>vec, xlims=(-5,5), ylims=(-5,5))
+        display(p1)
+    end
+end
+
+
 #anim = @animate for i in 1:2000
 #    ReverseDiff.reset!(xs)
 #    ReverseDiff.reset!(ys)
@@ -146,30 +168,11 @@ forward(network,x,y) = network(track([x,y])).data[1]
 #    l = train_step!(loss, network, xs, ys, λ=0.01)
 #    if mod1(i,50) == 1
 #        @info i l
-#        p1 = contour(-4:0.3:4, -2:0.3:2, f, fill=true, c=color_scheme, xlabel="x", ylabel="y", title="Truth")
-#        p2 = contour(-4:0.3:4, -2:0.3:2, (x,y)->forward(network,x,y), fill=true, c=color_scheme, xlabel="x",title="Iteration: $i")
-#        p = plot(p1,p2,size=(1200,400)) |> display
+#        ȳs = argmax(network(xs).data, dims=1)
+#        ȳs = [y.I[1] for y in ȳs]
+#        p2 = scatter(xs[1,:].data, xs[2,:].data, c=vec(ȳs), xlims=(-5,5), ylims=(-5,5))
+#        display(p2)
 #    end
 #end every 50
 #
 #gif(anim, "anim.gif", fps=15)
-
-for i in 1:2000
-    ReverseDiff.reset!(xs)
-    ReverseDiff.reset!(ys)
-    ReverseDiff.reset!(network)
-    l = train_step!(loss, network, xs, ys, λ=0.0001)
-    if mod1(i,50) == 1
-        @info i l
-        ȳs = argmax(network(xs).data, dims=1)
-        ȳs = [y.I[1] for y in ȳs]
-        display(network(xs))
-        display(ys)
-        display(ȳs)
-        p = scatter(xs[1,:].data, xs[2,:].data, c=vec(ȳs))
-        display(p)
-        # p1 = contour(-4:0.3:4, -2:0.3:2, f, fill=true, c=color_scheme, xlabel="x", ylabel="y", title="Truth")
-        # p2 = contour(-4:0.3:4, -2:0.3:2, (x,y)->forward(network,x,y), fill=true, c=color_scheme, xlabel="x",title="Iteration: $i")
-        # p = plot(p1,p2,size=(1200,400)) |> display
-    end
-end
--- src/Scientific_Programming_in_Julia.jl
+++ src/Scientific_Programming_in_Julia.jl
@@ -3,7 +3,7 @@ module Scientific_Programming_in_Julia
 include("ReverseDiff.jl")
 using .ReverseDiff
 export track, accum!, σ
-export TrackedArray, TrackedMatrix, TrackedVector
+export TrackedArray, TrackedMatrix, TrackedVector, TrackedReal
 
 include("Ecosystem.jl")
 
