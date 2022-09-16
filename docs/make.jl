649e05e294052049ca44607c98e59d016fa1e7b4
Niklas Heim <heim.niklas@gmail.com>
2022-09-16 10:09:14 +0200

    remove all lectures except 01/02

--- docs/make.jl
+++ docs/make.jl
@@ -51,70 +51,6 @@ lecture_02 = [
     "Homework" => "./lecture_02/hw.md"
 ]
 
-lecture_03 = [
-    "Lecture" => "./lecture_03/lecture.md"
-    "Lab" => "./lecture_03/lab.md"
-    "Homework" => "./lecture_03/hw.md"
-]
-
-lecture_04 = [
-    "Lecture" => "./lecture_04/lecture.md"
-    "Lab" => "./lecture_04/lab.md"
-    "Homework" => "./lecture_04/hw.md"
-]
-
-lecture_05 = [
-    "Lecture" => "./lecture_05/lecture.md"
-    "Lab" => "./lecture_05/lab.md"
-    "Homework" => "./lecture_05/hw.md"
-]
-
-lecture_06 = [
-    "Lecture" => "./lecture_06/lecture.md"
-    "Lab" => "./lecture_06/lab.md"
-    "Homework" => "./lecture_06/hw.md"
-]
-
-lecture_07 = [
-    "Lecture" => "./lecture_07/lecture.md"
-    "Lab" => "./lecture_07/lab.md"
-    "Homework" => "./lecture_07/hw.md"
-]
-
-lecture_08 = [
-    "Lecture" => "./lecture_08/lecture.md"
-    "Lab" => "./lecture_08/lab.md"
-    "Homework" => "./lecture_08/hw.md"
-]
-
-lecture_09 = [
-    "Lecture" => "./lecture_09/lecture.md"
-    "Lab" => "./lecture_09/lab.md"
-]
-
-lecture_10 = [
-    "Lecture" => "./lecture_10/lecture.md"
-    "Lab" => "./lecture_10/lab.md"
-    "Homework" => "./lecture_10/hw.md"
-]
-
-lecture_11 = [
-    "Lecture" => "./lecture_11/lecture.md"
-    "Lab" => "./lecture_11/lab.md"
-]
-
-lecture_12 = [
-    "Lecture" => "./lecture_12/lecture.md"
-    "Lab" => "./lecture_12/lab.md"
-    "Homework" => "./lecture_12/hw.md"
-]
-
-lecture_13 = [
-    "Lab" => "./lecture_13/lab.md"
-    "Lecture" => "./lecture_13/lecture.md"
-]
-
-
 makedocs(;
     modules = [Scientific_Programming_in_Julia],
     authors = "JuliaTeachingCTU",
@@ -133,18 +69,6 @@ makedocs(;
         "Projects" => "projects.md",
         "1: Introduction" => lecture_01,
         "2: The power of Type System & multiple dispatch" => lecture_02,
-        "3: Design patterns" => lecture_03,
-        "4: Packages development, Unit Tests & CI" => lecture_04,
-        "5: Benchmarking, profiling, and performance gotchas" => lecture_05,
-        "6: Language introspection" => lecture_06,
-        "7: Macros" => lecture_07,
-        "8: Introduction to automatic differentiation" => lecture_08,
-        "9: Manipulating intermediate representation" => lecture_09,
-        "10: Different levels of parallel programming" => lecture_10,
-        "11: Julia for GPU programming" => lecture_11,
-        "12: Uncertainty propagation in ODE" => lecture_12,
-        "13: Learning ODE from data" => lecture_13,
-        #"How to submit homeworks" => "how_to_submit_hw.md",
     ],
 )
 
