6952d29bb53b5280e6d3b1f2c660627de1cf95d4
Niklas Heim <heim.niklas@gmail.com>
2022-10-15 17:31:15 +0200

    legend

--- docs/make.jl
+++ docs/make.jl
@@ -66,8 +66,8 @@ lecture_04 = [
 lecture_05 = [
     "Lecture" => "./lecture_05/lecture.md"
     "Lab" => "./lecture_05/lab.md"
-    "Homework" => "./lecture_05/hw.md"
     "Ecosystem" => "./lecture_05/ecosystems.md"
+    "Homework" => "./lecture_05/hw.md"
 ]
 
 makedocs(;
