a4e964cc94e8a9baf0193fb84a130a03827036d0
Niklas Heim <heim.niklas@gmail.com>
2023-10-13 12:06:57 +0200

    remove lab.jl

--- docs/src/lecture_02/lab.jl
+++ docs/src/lecture_02/lab.jl
@@ -1,26 +1,40 @@
-include("Lab02Ecosystem.jl")
+abstract type Species end
+
+abstract type PlantSpecies <: Species end
+abstract type Grass <: PlantSpecies end
+
+abstract type AnimalSpecies <: Species end
+abstract type Sheep <: AnimalSpecies end
+abstract type Wolf <: AnimalSpecies end
+
+abstract type Agent{S<:Species} end
+
+# instead of Symbols we can use an Enum for the sex field
+# using an Enum here makes things easier to extend in case you
+# need more than just binary sexes and is also more explicit than
+# just a boolean
+@enum Sex female male
 
 
-w = World([Wolf(2), Grass(1), Grass(3)])
-# World{Agent}
-#   🐺♂ #2 E=10.0 ΔE=8.0 pr=0.1 pf=0.2
-#   🌿  #3 60% grown
-#   🌿  #1 100% grown
-
-
-## a> agent_count(w)
-# Dict{Symbol, Real} with 2 entries:
-#   :Animal => 1
-#   :Plant  => 1.6
-
-agent_count(x::Animal) = 1
-agent_count(x::Plant) = x.size / x.max_size
-
-function agent_count(w::World)
-    counts = Dict()
-    for (_,a) in w.agents
-        n = a |> typeof |> nameof
-        haskey(counts, n) ? counts[n] += agent_count(a) : counts[n] = agent_count(a)
-    end
-    return counts
+mutable struct Animal{A<:AnimalSpecies} <: Agent{A}
+    const id::Int
+    energy::Float64
+    const Δenergy::Float64
+    const reprprob::Float64
+    const foodprob::Float64
+    const sex::Sex
 end
+
+function Base.show(io::IO, a::Animal{A}) where {A<:AnimalSpecies}
+    e = a.energy
+    d = a.Δenergy
+    pr = a.reprprob
+    pf = a.foodprob
+    s = a.sex == female ? "♀" : "♂"
+    print(io, "$A$s #$(a.id) E=$e ΔE=$d pr=$pr pf=$pf")
+end
+
+# note that for new species/sexes we will only have to overload `show` on the
+# abstract species/sex types like below!
+Base.show(io::IO, ::Type{Sheep}) = print(io,"🐑")
+Base.show(io::IO, ::Type{Wolf}) = print(io,"🐺")
