67e78840a2fc1d2661b092a9f11b3f2dc9c9e4d2
Niklas Heim <heim.niklas@gmail.com>
2023-10-26 13:21:46 +0200

    bench plot

--- docs/src/lecture_05/jax-numpy-julia/poly-julia.jl
+++ docs/src/lecture_05/jax-numpy-julia/poly-julia.jl
@@ -36,17 +36,23 @@ tt_ts = map(sizes) do n
 end
 
 
-using Plots, DelimitedFiles
-p1 = plot(
-    sizes, j_ts,
-    label="Julia", yscale=:log10, xscale=:log10,
-    title="f(x) = 3x³ + 2x² + x + 1",
-    ylabel="μs", xlabel="x-size",
+using CairoMakie, DelimitedFiles
+fig = Figure()
+ax = Axis(
+    fig[1,1], title="f(x) = 3x³ + 2x² + x + 1",
     xticks=sizes, yticks=[0.1,1,10,100,1000,10000],
-    lw=2, legend=:topleft
+    xscale=log10, yscale=log10,
+    ylabel="μs", xlabel="x [x=rand(n)]"
 )
-plot!(p1, sizes, t_ts, label="@turbo")
-plot!(p1, sizes, tt_ts, label="@tturbo")
-plot!(p1, sizes, readdlm("numpy.txt", Float64), label="Numpy")
-plot!(p1, sizes, readdlm("jax.txt", Float64), label="JAX")
-savefig(p1, "bench.png")
+lines!(ax, sizes, j_ts, linewidth=2)
+scatter!(ax, sizes, j_ts, label="Julia", marker=:circle)
+lines!(ax, sizes, t_ts, linewidth=2)
+scatter!(ax, sizes, t_ts, label="@turbo", marker=:rect)
+lines!(ax, sizes, tt_ts, linewidth=2)
+scatter!(ax, sizes, tt_ts, label="@tturbo", marker=:star5)
+lines!(ax, sizes, readdlm("numpy.txt", Float64)|>vec, linewidth=2)
+scatter!(ax, sizes, readdlm("numpy.txt", Float64)|>vec, label="Numpy", marker=:diamond)
+lines!(ax, sizes, readdlm("jax.txt", Float64)|>vec, linewidth=2)
+scatter!(ax, sizes, readdlm("jax.txt", Float64)|>vec, label="JAX", marker=:hexagon)
+axislegend(position=:lt)
+save("bench.png", fig)
