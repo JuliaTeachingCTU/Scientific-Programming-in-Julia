ce37f680ad294cd6503a1f515b0ebb6b8476f680
Documenter.jl <documenter@juliadocs.github.io>
2023-10-26 15:36:32 +0000

    build based on d7f1d20

--- dev/lecture_05/jax-numpy-julia/poly-julia.jl
+++ dev/lecture_05/jax-numpy-julia/poly-julia.jl
@@ -1,58 +1,65 @@
 using BenchmarkTools
 using LoopVectorization
+using CairoMakie
+using DelimitedFiles
+
+# !!! RUN THIS WITH:  julia -t 4
+RERUN = false
 
 function f(x)
     @. 3*x^3 + 2*x^2 + x + 1
 end
 
-function ft(x)
+function f_turbo(x)
     @turbo @. 3*x^3 + 2*x^2 + x + 1
 end
 
-function g(x)
+function f_tturbo(x)
     @tturbo @. 3*x^3 + 2*x^2 + x + 1
 end
 
-sizes = [10,100,1000,10000, 100000, 1000000, 10000000]
-j_ts = map(sizes) do n
-    x = rand(n)
-    r = @benchmark f($x)
-    display(r)
-    minimum(r.times) ./ 10^3
+sizes = [10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000]
+funcs = [f, f_turbo, f_tturbo]
+
+if RERUN
+    times = map(funcs) do fn
+        ts = map(sizes) do n
+            x = rand(n)
+            r = @benchmark $fn($x)
+            t = minimum(r.times) ./ 10^3
+            @info "Benchmarking" fn n t
+            t
+        end
+        writedlm("julia-$(nameof(fn)).txt", ts)
+        nameof(fn) => ts
+    end |> Dict
+else
+    times = Dict(nameof(fn)=>readdlm("julia-$(nameof(fn)).txt", Float64)|>vec for fn in funcs)
 end
 
-t_ts = map(sizes) do n
-    x = rand(n)
-    r = @benchmark ft($x)
-    display(r)
-    minimum(r.times) ./ 10^3
-end
-
-tt_ts = map(sizes) do n
-    x = rand(n)
-    r = @benchmark g($x)
-    display(r)
-    minimum(r.times) ./ 10^3
-end
-
-
-using CairoMakie, DelimitedFiles
 fig = Figure()
 ax = Axis(
-    fig[1,1], title="f(x) = 3x³ + 2x² + x + 1",
+    fig[1,1],
+    title=L"f(x) = 3x^3 + 2x^2 + x + 1",
+    ylabel=L"\mu s", xlabel=L"$n$ where $[x = \text{rand}(n)]$",
     xticks=sizes, yticks=[0.1,1,10,100,1000,10000],
     xscale=log10, yscale=log10,
-    ylabel="μs", xlabel="x [x=rand(n)]"
 )
-lines!(ax, sizes, j_ts, linewidth=2)
-scatter!(ax, sizes, j_ts, label="Julia", marker=:circle)
-lines!(ax, sizes, t_ts, linewidth=2)
-scatter!(ax, sizes, t_ts, label="@turbo", marker=:rect)
-lines!(ax, sizes, tt_ts, linewidth=2)
-scatter!(ax, sizes, tt_ts, label="@tturbo", marker=:star5)
+
+lines!(ax, sizes, times[:f], linewidth=2)
+scatter!(ax, sizes, times[:f], label="Julia", marker=:circle)
+
+lines!(ax, sizes, times[:f_turbo], linewidth=2)
+scatter!(ax, sizes, times[:f_turbo], label="@turbo", marker=:rect)
+
+lines!(ax, sizes, times[:f_tturbo], linewidth=2)
+scatter!(ax, sizes, times[:f_tturbo], label="@tturbo", marker=:star5)
+
 lines!(ax, sizes, readdlm("numpy.txt", Float64)|>vec, linewidth=2)
 scatter!(ax, sizes, readdlm("numpy.txt", Float64)|>vec, label="Numpy", marker=:diamond)
+
 lines!(ax, sizes, readdlm("jax.txt", Float64)|>vec, linewidth=2)
 scatter!(ax, sizes, readdlm("jax.txt", Float64)|>vec, label="JAX", marker=:hexagon)
+
 axislegend(position=:lt)
 save("bench.png", fig)
