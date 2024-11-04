c34db8238a056dadebc209eb1a32a1e0825e8a2a
Václav Mácha <vaclav.macha@gendigital.com>
2024-11-04 13:48:13 +0100

    Update build script

--- docs_vitepress/run_build_local.sh
+++ docs_vitepress/run_build_local.sh
@@ -1,3 +1,7 @@
+# install all 
+npm install vitepress dependencies
+
+# run julia server and build documentation
 julia -e 'using LiveServer; servedocs(foldername=pwd())' --project="." &
 julia -e 'using DocumenterVitepress: dev_docs; dev_docs("build", md_output_path="")' --project="." &
 
