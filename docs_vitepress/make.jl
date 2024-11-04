d87735697143d3bd3397f827ae9676444bbf2a8c
Václav Mácha <vaclav.macha@gendigital.com>
2024-11-04 15:58:41 +0100

    Add lecture 5

--- docs_vitepress/make.jl
+++ docs_vitepress/make.jl
@@ -51,11 +51,11 @@ pages = [
             "Lab" => "lab.md",
             "Homework" => "hw.md",
         ]),
-        # "5: Performance benchmarking" => add_prefix("lecture_05", [
-        #     "Lecture" => "lecture.md",
-        #     "Lab" => "lab.md",
-        #     "Homework" => "hw.md",
-        # ]),
+        "5: Performance benchmarking" => add_prefix("lecture_05", [
+            "Lecture" => "lecture.md",
+            "Lab" => "lab.md",
+            "Homework" => "hw.md",
+        ]),
         # "6: Lanuage introspection" => add_prefix("lecture_06", [
         #     "Lecture" => "lecture.md",
         #     "Lab" => "lab.md",
