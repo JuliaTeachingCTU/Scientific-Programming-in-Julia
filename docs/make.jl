a7ca8f2459f92216d4220afd852b1bb5d8a20695
Tomas Pevny <pevnak@gmail.com>
2024-11-22 13:55:19 +0100

    fixed path to lab

--- docs/make.jl
+++ docs/make.jl
@@ -88,7 +88,7 @@ pages = [
     "9: Intermediate representation" => [
         "Lecture v.2 (newest)" => "./lecture_09/lecture.md",
         "Lecture v.1 (older)" => "./lecture_09_v1/lecture.md",
-        "Lab" => "./lecture_09/lab.md",
+        "Lab" => "./lecture_09_v1/lab.md",
     ],
 
     "10: Parallel programming" => [
