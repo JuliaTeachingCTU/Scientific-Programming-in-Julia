a3a4bad4448ebf12c7265cd047e7945ef0b4130c
pevnak <pevnak@protonmail.ch>
2025-11-20 20:30:18 +0100

    lecture_08

--- docs/make.jl
+++ docs/make.jl
@@ -48,7 +48,7 @@ pages = [
         "4: Package development, unit tests & CI" => "lecture_04/lecture.md",
         "5: Performance benchmarking" => "lecture_05/lecture.md",
         "6: Language introspection" => "lecture_06/lecture.md",
-        "7: Macros" => "lecture_07/lecture.md"
+        "7: Macros" => "lecture_07/lecture.md",
         "8: Automatic differentiation 1" => "lecture_08/lecture.md"
     ]),
     "Labs" => add_prefix("./lectures", [
@@ -58,7 +58,7 @@ pages = [
         "4: Package development, unit tests & CI" => "lecture_04/lab.md",
         "5: Performance benchmarking" => "lecture_05/lab.md",
         "6: Language introspection" => "lecture_06/lab.md",
-        "7: Macros" => "lecture_07/lab.md"
+        "7: Macros" => "lecture_07/lab.md",
         "8: Automatic differentiation 1" => "lecture_08/lab.md"
     ]),
     "Homeworks" => add_prefix("./lectures", [
@@ -68,7 +68,7 @@ pages = [
         "4: Package development, unit tests & CI" => "lecture_04/hw.md",
         "5: Performance benchmarking" => "lecture_05/hw.md",
         "6: Language introspection" => "lecture_06/hw.md",
-        "7: Macros" => "lecture_07/hw.md"
+        "7: Macros" => "lecture_07/hw.md",
         "8: Automatic differentiation 1" => "lecture_08/hw.md"
     ]),
 
